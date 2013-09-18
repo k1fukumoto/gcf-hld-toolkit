7da5ce269997a37033ff608753f369cd3df93d64
Kaoru Fukumoto <k1fukumoto@gmail.com>
2013-09-19 08:48:58 +0900

    initial commit

---
+++ script/GCF-VerifyInventory.ps1
@@ -0,0 +1,13 @@
+﻿#######################################################################################
+# Copyright 2013 VCE All Rights Reserved
+#
+# You may freely use and redistribute this script as long as this 
+# copyright notice remains intact 
+#
+#
+# DISCLAIMER. THIS SCRIPT IS PROVIDED TO YOU "AS IS" WITHOUT WARRANTIES OR CONDITIONS 
+# OF ANY KIND, WHETHER ORAL OR WRITTEN, EXPRESS OR IMPLIED. THE AUTHOR SPECIFICALLY 
+# DISCLAIMS ANY IMPLIED WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY 
+# QUALITY, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE. 
+#
+#######################################################################################
