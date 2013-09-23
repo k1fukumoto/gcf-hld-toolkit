3762f200fc61c1e6d8543fd04088d5cfb60b5608
Kaoru Fukumoto <k1fukumoto@gmail.com>
2013-09-24 08:55:41 +0900

    after first successful verify vm run

--- script/verify_vm_inventory.py
+++ script/verify_vm_inventory.py
@@ -1,27 +1,40 @@
 #!/usr/bin/python
 
 import csv
-import xml.etree.ElementTree as et
-
-et.parse('./data/GCF-Servicde.xml')
-
+import re
+import os
 from pysphere import VIServer
+
+def log(l):
+    print(l)
+    with open('./log/verify_vm_inventory.log','a') as f: f.write(l+"\n")
+    
+def INFO(s):
+    log("INFO: %s" % s)
+
+def ERROR(s):
+    log("ERROR: %s" % s)
+
+try: os.remove('./log/verify_vm_inventory.log')
+except: ''
 
 csv = csv.reader(open('./data/GCF-VM.csv'))
 
 vc = VIServer()
 cur_region = None
 for row in csv:
-    region = row[0]
-    appid = row[1]
-    vm = row[2]
+    appid = row[0]
+    vm = row[1]
+
+    region = ''
+    m = re.match('.*(TOKYO|OSAKA)',appid)
+    if(m): region = m.group(1)
 
     if(cur_region != region):
         vcip = None
         if(region == 'TOKYO'):
             vcip = '10.1.1.50'
         else:
-            continue
             vcip = '10.1.8.50'
     
         print("INFO: Trying to connect %s" % vcip)
@@ -32,10 +45,15 @@ for row in csv:
     try:
         vmo = vc.get_vm_by_name(vm)
     except:
-        print ("ERROR: VM not found %-16s (%s)" % (appid,vm))
+        ERROR ("VM not found %-16s (%s)" % (appid,vm))
         continue
 
-    if('POWERED ON' == vmo.get_status()):
-        print ("INFO: Verified %-16s (%s)" % (appid,vm))
-    else:
-        print ("ERROR: VM not running %-16s (%s)" % (appid,vm))
+    try:
+        if('POWERED ON' == vmo.get_status()):
+            INFO ("Verified %-16s (%s)" % (appid,vm))
+        else:
+            ERROR ("VM not running %-16s (%s)" % (appid,vm))
+    except:
+        ERROR("VM query error %-16s (%s)" % (appid,vm))
+        continue
+        
