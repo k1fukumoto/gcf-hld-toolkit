c8e1fc4e975b7c9a6233fbbe6a7b675f4ff9e1d9
Kaoru Fukumoto <k1fukumoto@gmail.com>
2013-09-24 16:00:57 +0900

    Completing the initial replica implementation

--- script/compile_app_code.py
+++ script/compile_app_code.py
@@ -23,13 +23,13 @@ def on_appcode(code):
         print ("%s,%s" % (code,appid_d[code]))
     else:
         print 
-        m = re.match("\S+-\S+-\S+-\S+-(\S+)_MGMT_REPLICA_(VB\d{2})-(\S+)",code) 
+        m = re.match("\S+-\S+-\S+-\S+-(\S+)_MGMT_REPLICA_(VB\d{2})-(\S+)$",code) 
         if(not m): raise Exception("ERROR appcode %s not found"  % code)
         
         rep = m.group(1)
         vb = m.group(2)
         ac = m.group(3)
-        pat = "%s-%s_MGMT-%s" %(vb,rep,ac)
+        pat = "%s-%s_MGMT-%s$" %(vb,rep,ac)
         for ac in appid_d.keys():
             if(re.match('.*' + pat,ac)):
                 print ("%s,%s" % (code,appid_d[ac]))
@@ -42,24 +42,34 @@ for pod in pods:
         for dc in region:
             for vb in dc:
                 for mod in vb.findall('Module'):
-                    opt_app = False
-                    if('Option' in mod.attrib): opt_app = mod.attrib['Option']
+                    # Build dictionary for optional applications
+                    opt_app = {}
+                    for opt in mod.findall('Option'):
+                        opt_app[code(opt)] = True
+
+                    # Iterate through all applications in module
                     for app in mod_d[code(mod)]:
+                        # If application is optional, check if it is in optional app dictionary. Otherwise skip.
                         if('Optional' in app.attrib):
-                            if(code(app) != opt_app): 
+                            if(not code(app) in opt_app): 
                                 continue
+                        # Iterate through each node in application
                         for node in app:
                             nodestr = code(node)
+                            # Add node suffix, only when it is not empty
                             if (len(nodestr) > 0):
                                 nodestr = "-" + nodestr
+                            # If "Primary" is specified, append it to module
                             modstr = code(mod)
                             if('Primary' in mod.attrib):
                                 modstr = "%s_%s" % (modstr,mod.attrib['Primary'])
+
                             codestr = ("%s-%s-%s-%s-%s-%s%s" % (code(pod),code(region),code(dc),code(vb),modstr,code(app),nodestr))
+
+                            # If "Count" is specified in module, iterete through it with numeric suffix
                             if ('Count' in mod.attrib):
                                 for i in range(int(mod.attrib['Count'])):
                                     on_appcode ("%s-%02d" % (codestr,i+1))
                             else:
                                 on_appcode (codestr)
              
-    
